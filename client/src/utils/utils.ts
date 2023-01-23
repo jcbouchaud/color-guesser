4325e14653011b67b89a3fe714092f94d8eb549e
jcbouchaud <jcbouchaud.sh@gmail.com>
2023-01-23 23:22:37 +0100

    remove useless func

--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -9,39 +9,6 @@ const genRandomHexColor = () => {
   return "#" + n.slice(0, 6);
 };
 
-export const hexToHSL = (hex: string) => {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  if (result && result.length >= 3) {
-    const r = parseInt(result[1], 16) / 255;
-    const g = parseInt(result[2], 16) / 255;
-    const b = parseInt(result[3], 16) / 255;
-    const max = Math.max(r, g, b);
-    const min = Math.min(r, g, b);
-    let h = (max + min) / 2;
-    let s = (max + min) / 2;
-    let l = (max + min) / 2;
-    if (max === min) {
-      h = s = 0;
-    } else {
-      const d = max - min;
-      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-      switch (max) {
-        case r:
-          h = (g - b) / d + (g < b ? 6 : 0);
-          break;
-        case g:
-          h = (b - r) / d + 2;
-          break;
-        case b:
-          h = (r - g) / d + 4;
-          break;
-      }
-      h /= 6;
-    }
-    return { h, s, l };
-  }
-};
-
 const setNewRound = (length: number) => {
   const choices: string[] = [];
 
